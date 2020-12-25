114241d82f21e6f4e8f4c5781b784ad4ec010038
pavel asadchy <asadchy.pavel@gmail.com>
2020-12-25 15:02:54 +0300

    feat: login user endpoint

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -15,50 +15,93 @@ router.post(
       .isLength({ min: 6 }),
   ],
   async (request, response) => {
-  try {
+    try {
 
-    // Getting validation results
-    const errors = validationResult(request);
-    if (!errors.isEmpty()) {
-      return response.status(400).json({
-        errors: errors.array(),
-        message: 'Incorrect registration inputs'
-      })
-    }
+      // Getting validation results
+      const errors = validationResult(request);
+      if (!errors.isEmpty()) {
+        return response.status(400).json({
+          errors: errors.array(),
+          message: 'Incorrect registration inputs'
+        });
+      }
 
-    // Getting request fiels sent from frontend
-    const { email, password } = request.body;
+      // Getting request fiels sent from frontend
+      const { email, password } = request.body;
 
-    const candidate = await User.findOne({ email });
+      const candidate = await User.findOne({ email });
 
-    if (candidate) {
-      return response.status(400).json({
-        message: 'User already exists'
+      if (candidate) {
+        return response.status(400).json({
+          message: 'User already exists'
+        });
+      }
+
+      // Hashing password (12 - salt for encryption )
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new User({ email, password: hashedPassword });
+
+      await user.save();
+
+      response.status(201).json({
+        message: 'User saved'
+      });
+
+    } catch(err) {
+
+      // Setting message in case of error
+      response.status(500).json({
+        message: 'Something went wrong, please try again'
       });
     }
-
-    // Hashing password (12 - salt for encryption )
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const user = new User({ email, password: hashedPassword });
-
-    await user.save();
-
-    response.status(201).json({
-      message: 'User saved'
-    });
-
-  } catch(err) {
-
-    // Setting message in case of error
-    response.status(500).json({
-      message: 'Something went wrong, please try again'
-    });
   }
-})
+)
 
 // /api/auth/login
-router.post('/login', async (request, response) => {
+router.post(
+  '/login',
+  [
+    check('email', 'Enter correct email')
+      .normalizeEmail().isEmail(),
+    check('password', 'Enter the password').exists(),
+  ],
+  async (request, response) => {
+    try {
 
-})
+      const errors = validationResult(request);
+      if (!errors.isEmpty()) {
+        return response.status(400).json({
+          errors: errors.array(),
+          message: 'Incorrect authorization inputs'
+        });
+      }
+
+      const { email, password } = request.body;
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return response.status(400).json({
+          message: 'User not found',
+        });
+      }
+
+      // Comparing user password & saved (hashed) passwords
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return response.status(400).json({
+          message: 'Incorrect password, please try again'
+        });
+      }
+  
+    } catch(err) {
+  
+      // Setting message in case of error
+      response.status(500).json({
+        message: 'Something went wrong, please try again'
+      });
+    }
+  }
+)
 
 module.exports = router;
