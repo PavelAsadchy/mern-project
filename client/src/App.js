19c2ce506a22cccffb4ac1b9a6e2a568980f6993
pavel asadchy <asadchy.pavel@gmail.com>
2020-12-27 22:13:38 +0300

    feat: auth context

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
 import 'materialize-css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthContext } from './context/AuthContext';
 import { useAuth } from './hooks/auth.hook';
 import { useRoutes } from './routes';
 
 function App() {
   const { login, logout, token, userId } = useAuth();
-  const routes = useRoutes(false);
+  const isAuthenticated = !!token;
+  const routes = useRoutes(isAuthenticated);
   return (
-    <Router>
-      <div className="container">
-        { routes }
-      </div>
-    </Router>
+    <AuthContext.Provider value={{
+      token, userId, login, logout, isAuthenticated
+    }}>
+      <Router>
+        <div className="container">
+          { routes }
+        </div>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
