e3d50760ed3a64d27eb66be783cea7f038d79184
pavel asadchy <asadchy.pavel@gmail.com>
2020-12-27 13:06:39 +0300

    feat: register handler on auth page

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,12 +1,22 @@
 import { useState } from 'react';
+import { useHttp } from '../hooks/http.hook';
 
 export const AuthPage = () => {
+  const { loading, request, error } = useHttp();
+
   const [ form, setForm ] = useState({
     email: '', password: ''
   });
 
   const changeHandler = event => {
     setForm({ ...form, [event.target.name]: event.target.value })
+  };
+
+  const registerHandler = async () => {
+    try {
+      const data = await request('/api/auth/register', 'POST', { ...form });
+      console.log(data);
+    } catch (err) {}
   };
 
   return (
@@ -43,8 +53,20 @@ export const AuthPage = () => {
             </div>
           </div>
           <div className="card-action">
-            <button className="btn yellow darken-1" style={{ marginRight: 10 }}>Log in</button>
-            <button className="btn grey lighten-1 black-text">Sign up</button>
+            <button
+              className="btn yellow darken-1"
+              style={{ marginRight: 10 }}
+              disabled={ loading }
+            >
+              Log in
+            </button>
+            <button
+              className="btn grey lighten-1 black-text"
+              onClick={ registerHandler }
+              disabled={ loading }
+            >
+              Sign up
+            </button>
           </div>
         </div>
       </div>
