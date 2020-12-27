fa5bc7d608bb5c7bdd5367b2fb9e2b17d40f4984
pavel asadchy <asadchy.pavel@gmail.com>
2020-12-27 15:54:46 +0300

    feat: login & auth handling on backend, error handling

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,12 +1,18 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useHttp } from '../hooks/http.hook';
+import { useMessage } from '../hooks/message.hook';
 
 export const AuthPage = () => {
-  const { loading, request, error } = useHttp();
-
+  const message = useMessage();
+  const { loading, request, error, clearError } = useHttp();
   const [ form, setForm ] = useState({
     email: '', password: ''
   });
+
+  useEffect(() => {
+    message(error);
+    clearError();
+  }, [error, message, clearError])
 
   const changeHandler = event => {
     setForm({ ...form, [event.target.name]: event.target.value })
@@ -15,7 +21,14 @@ export const AuthPage = () => {
   const registerHandler = async () => {
     try {
       const data = await request('/api/auth/register', 'POST', { ...form });
-      console.log(data);
+      message(data.message);
+    } catch (err) {}
+  };
+
+  const loginHandler = async () => {
+    try {
+      const data = await request('/api/auth/login', 'POST', { ...form });
+      message(data.message);
     } catch (err) {}
   };
 
@@ -56,6 +69,7 @@ export const AuthPage = () => {
             <button
               className="btn yellow darken-1"
               style={{ marginRight: 10 }}
+              onClick={ loginHandler }
               disabled={ loading }
             >
               Log in
