1a0e83552506b030671aa8661f4bf6ea2b09ac6f
pavel asadchy <asadchy.pavel@gmail.com>
2021-01-01 21:45:35 +0300

    feat: detail page

--- client/src/pages/DetailPage.js
+++ client/src/pages/DetailPage.js
@@ -1,11 +1,38 @@
-import { useState } from 'react';
+import { useCallback, useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Loader } from '../components/Loader';
+import { AuthContext } from '../context/AuthContext';
+import { useHttp } from '../hooks/http.hook';
 
 export const DetailPage = () => {
+  const { token } = useContext(AuthContext);
+  const { request, loading } = useHttp();
   const [link, setLink] = useState(null);
+  const linkId = useParams().id; // Name "id" comes from route path /detail/:id
+
+  const getLink = useCallback(async() => {
+    try {
+      const fetched = await request(
+        `/api/link/${linkId}`,
+        'GET',
+        null,
+        { Authorization: `Bearer ${token}`}
+      );
+      setLink(fetched);
+    } catch (err) {}
+  }, [token, linkId, request]);
+
+  useEffect(() => {
+    getLink();
+  }, [getLink]);
+
+  if (loading) {
+    return <Loader />;
+  }
 
   return (
-    <div>
-      <h1>Detail Page</h1>
-    </div>
+    <>
+      { !loading && link && <LinkCard />}
+    </>
   );
 }
