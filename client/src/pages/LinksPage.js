d89368954bfa1830765e4f36ac6575595c42adcf
pavel asadchy <asadchy.pavel@gmail.com>
2021-01-01 22:39:52 +0300

    feat: linksList component

--- client/src/pages/LinksPage.js
+++ client/src/pages/LinksPage.js
@@ -1,20 +1,21 @@
 import { useCallback, useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../context/AuthContext';
 import { Loader } from '../components/Loader';
+import { LinksList } from '../components/LinksList';
 import { useHttp } from '../hooks/http.hook';
 
 export const LinksPage = () => {
-  const [links, setLinks] = useState();
+  const [links, setLinks] = useState([]);
   const { request, loading } = useHttp();
   const { token } = useContext(AuthContext);
 
   const fetchLinks = useCallback(async () => {
     try {
-      const fetched = request(
+      const fetched = await request(
         '/api/link',
         'GET',
         null,
-        { Authorized: `Bearer ${token}`}
+        { Authorization: `Bearer ${token}`}
       );
       setLinks(fetched);
     } catch (err) {}
@@ -27,6 +28,7 @@ export const LinksPage = () => {
   if (loading) {
     return <Loader />;
   }
+  console.log(links)
 
   return (
     <>
